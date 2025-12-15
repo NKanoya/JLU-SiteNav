6b6bd41746d05ef925fd72f0c35df39c96b9c52f
NKanoya <3397540942@qq.com>
2025-12-15 23:20:35 +0800

    [feat] 新增悬浮按钮的悬浮框提示显示
    - 为 'Github' 和 '显示模式切换' 按钮添加了对应悬浮框的内容显示

--- src/components/floating-button/FloatingButtonBox.vue
+++ src/components/floating-button/FloatingButtonBox.vue
@@ -1,21 +1,42 @@
-<script setup>
+<script setup lang="ts">
     import FloatingButton from './FloatingButton.vue'
     import Github from "@/components/icon/Github.vue";
     import Brightness from "@/components/icon/Brightness.vue";
 
     const toGithub = () => {
         window.open('https://github.com/NKanoya/JLU-SiteNav', '_blank');
-    }
+    };
 
     import { toggleTheme } from './brightness.js';
+    import {displayTooltip, hideTooltip, updateToolTipPosition} from '@/components/tooltip/tooltip-behaviour';
+
+    const tooltipSwitchTheme = () => {
+        const html = document.documentElement;
+        if(html.getAttribute('data-theme') == 'dark') {
+            displayTooltip({ newType: 'brightness', param: ['亮色']})
+        } else {
+            displayTooltip({ newType: 'brightness', param: ['暗色']})
+        }
+
+    };
 </script>
 
 <template>
     <div class="floating-button-box">
-        <FloatingButton @click="toGithub">
+        <FloatingButton
+            @click="toGithub"
+            @mouseenter="displayTooltip('github')"
+            @mouseleave="hideTooltip"
+            @mousemove="updateToolTipPosition"
+        >
             <Github />
         </FloatingButton>
-        <FloatingButton @click="toggleTheme">
+        <FloatingButton
+            @click="toggleTheme"
+            @mouseenter="tooltipSwitchTheme"
+            @mouseleave="hideTooltip"
+            @mousemove="updateToolTipPosition"
+        >
             <Brightness />
         </FloatingButton>
     </div>
